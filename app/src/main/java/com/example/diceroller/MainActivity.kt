ed3ee6ef3952490cc5e0e7bd573527b57dc175fb
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-07-07 13:53:40 +0200

    Add button component

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -34,7 +36,9 @@ fun DiceWithButtonAndImage(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text("TODO")
+        Button(onClick = { TODO("Bouton à implémenter") }) {
+            Text(stringResource(R.string.roll))
+        }
     }
 }
 @Preview
